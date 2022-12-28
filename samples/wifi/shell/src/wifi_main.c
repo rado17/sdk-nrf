0eccd0b2802b0848b9d5725d046bf83936b17abe
Krishna T <krishna.t@nordicsemi.no>
2022-12-28 13:28:04 +0530

    Add test bench for serial agent
    This is commented but handy in testing serial agent read from UART, with
    the current code tests fails with size >= 800 bytes.
    
    Signed-off-by: Krishna T <krishna.t@nordicsemi.no>

--- samples/wifi/shell/src/wifi_main.c
+++ samples/wifi/shell/src/wifi_main.c
@@ -8,6 +8,7 @@
  * @brief WiFi shell sample main function
  */
 
+#include <stdio.h>
 #include <zephyr/sys/printk.h>
 #include <nrfx_clock.h>
 #include <zephyr/device.h>
@@ -32,5 +33,26 @@ void main(void)
 	const struct device *dev = device_get_binding("wlan0");
 
 	net_config_init_app(dev, "Initializing network");
+#endif
+/* Test bench for serial agent */
+#if 0
+	for(int i = 0; i < 10; i++) {
+		size_t buf_size = 500 + i * 100;
+
+		printf("Trying buf size: %d\n", buf_size);
+
+		char *buf = malloc(buf_size);
+		if (!buf) {
+			printf("Failed to alloc: %d\n", buf_size);
+			continue;
+		}
+		memset(buf, 0XAA + i, buf_size);
+		printf("START\n");
+		for (int j = 0; j < buf_size; j++)
+			printf("%02X", buf[j]);
+		printf("END\n");
+		free(buf);
+	}
+	exit(0);
 #endif
 }
