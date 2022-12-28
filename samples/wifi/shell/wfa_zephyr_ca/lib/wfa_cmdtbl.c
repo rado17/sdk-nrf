f7b1e1ba49a1bf9537aeb5ecab1ec719556d93a0
Krishna T <krishna.t@nordicsemi.no>
2022-12-28 13:05:12 +0530

    Workaround for traffic receive stop response
    Traffic receive stop response is the largest one 520 * numStreams, this
    is causing issues with the serial agent, as anything > 800 bytes over
    UART is only partially read by the serial agent and leads to "function
    not defined" error in the WFA CA as the regex match fails.
    
    As a workaround remove the advanced Wi-Fi feature data structures from
    the DUT response union to decrease the overall size, with this the
    structure size reduces from 520 to ~260, and this works well with serial
    agent.
    
    We still need to maintain the TLV numbering, so, replace the removed
    functions with not defined.
    
    Signed-off-by: Krishna T <krishna.t@nordicsemi.no>

--- samples/wifi/shell/wfa_zephyr_ca/lib/wfa_cmdtbl.c
+++ samples/wifi/shell/wfa_zephyr_ca/lib/wfa_cmdtbl.c
@@ -119,47 +119,43 @@ xcCommandFuncPtr gWfaCmdFuncTbl[WFA_STA_COMMANDS_END] =
 #endif
     wfaStaDevSendFrame,          /*    WFA_STA_DEV_SEND_FRAME_TLV              (48)*/
     wfaStaSetSecurity,        /*    WFA_STA_SET_SECURITY_TLV           (49)*/
-    wfaStaGetP2pDevAddress,   /*    WFA_STA_GET_P2P_DEV_ADDRESS_TLV    (50)*/
-    wfaStaSetP2p,	      /*    WFA_STA_SET_P2P_TLV	               (51)*/
-    wfaStaP2pConnect,         /*    WFA_STA_P2P_CONNECT_TLV            (52)*/
-    wfaStaStartAutoGo, 	      /* WFA_STA_START_AUTO_GO                 (53)*/
-    wfaStaP2pStartGrpFormation, /*    WFA_STA_P2P_START_GRP_FORMATION_TLV      (54)*/
-
-    wfaStaP2pDissolve,          /*    WFA_STA_P2P_DISSOLVE_TLV                 (55)*/
-    wfaStaSendP2pInvReq,        /*    WFA_STA_SEND_P2P_INV_REQ_TLV             (56)*/
-    wfaStaAcceptP2pInvReq,	/*    WFA_STA_ACCEPT_P2P_INV_REQ_TLV           (57)*/
-    wfaStaSendP2pProvDisReq,    /*    WFA_STA_SEND_P2P_PROV_DIS_REQ_TLV    (58)*/
-    wfaStaSetWpsPbc,            /*    WFA_STA_SET_WPS_PBC_TLV              (59)*/
-
-    wfaStaWpsReadPin,           /*    WFA_STA_WPS_READ_PIN_TLV             (60)*/
-    wfaStaWpsEnterPin,          /*    WFA_STA_WPS_ENTER_PIN_TLV	           (61)*/
-    wfaStaGetPsk,               /*    WFA_STA_GET_PSK_TLV                  (62)*/
-    wfaStaP2pReset,             /*    WFA_STA_P2P_RESET_TLV                (63)*/
-    wfaStaWpsReadLabel,         /*    WFA_STA_WPS_READ_LABEL_TLV           (64)*/
-    wfaStaGetP2pIpConfig,       /*    WFA_STA_GET_P2P_IP_CONFIG_TLV        (65)*/
-    wfaStaSendServiceDiscoveryReq,    /*    WFA_STA_SEND_SERVICE_DISCOVERY_REQ_TLV                (66)*/
-    wfaStaSendP2pPresenceReq,         /*    WFA_STA_SEND_P2P_PRESENCE_REQ_TLV                     (67)*/
-    wfaStaSetSleepReq,                /*    WFA_STA_SEND_P2P_PRESENCE_REQ_TLV                     (68)*/
-    wfaStaSetOpportunisticPsReq,             /*    WFA_STA_SET_OPPORTUNISTIC_PS_TLV               (69)*/
-
-    wfaStaAddArpTableEntry,    /*    WFA_STA_ADD_ARP_TABLE_ENTRY_TLV      (70)*/
-    wfaStaBlockICMPResponse,    /*    WFA_STA_BLOCK_ICMP_RESPONSE_TLV      (71)*/
-    wfaStaSetRadio,           /*    WFA_STA_SET_RADIO_TLV              (72)*/
-    wfaStaSetRFeature,        /*    WFA_STA_RFEATURE_TLV               (73)*/
-
-	wfaStaStartWfdConnection,   /*    WFA_STA_START_WFD_CONNECTION_TLV               (74)*/
-	wfaStaCliCommand,         /*   WFA_STA_CLI_CMD_TLV            (75)*/
-	wfaStaConnectGoStartWfd,   /*    WFA_STA_CONNECT_GO_START_WFD_TLV               (76)*/
-	wfaStaGenerateEvent,         /*   WFA_STA_GENERATE_EVENT_TLV            (77)*/
-	wfaStaReinvokeWfdSession,         /*   WFA_STA_REINVOKE_WFD_SESSION_TLV            (78)*/
-	wfaStaGetParameter,         /*   WFA_STA_GET_PARAMETER_TLV            (79)*/
-	wfaStaNfcAction,         /*   WFA_STA_NFC_ACTION_TLV            (80)*/
-	wfaStaInvokeCommand,         /*   WFA_STA_INVOKE_COMMAND_TLV            (81)*/
-	wfaStaManageService,         /*   WFA_STA_MANAGE_SERVICE_TLV            (82)*/	
-	wfaStaGetEvents,         /*   WFA_STA_GET_EVENTS_TLV            (83)*/
-	wfaStaGetEventDetails,         /*   WFA_STA_GET_EVENT_DETAILS_TLV            (84)*/	
-	wfaStaExecAction,         /*   WFA_STA_EXEC_ACTION_TLV            (85)*/	
-	wfaStaScan,		/*	WFA_STA_SCAN_TLV		(86)*/
+    NotDefinedYet,            /*    None                               (0) */
+    NotDefinedYet,            /*    None                               (0) */
+    NotDefinedYet,            /*    None                               (0) */
+    NotDefinedYet,            /*    None                               (0) */
+    NotDefinedYet,            /*    None                               (0) */
+    NotDefinedYet,            /*    None                               (0) */
+    NotDefinedYet,            /*    None                               (0) */
+    NotDefinedYet,            /*    None                               (0) */
+    NotDefinedYet,            /*    None                               (0) */
+    NotDefinedYet,            /*    None                               (0) */
+    NotDefinedYet,            /*    None                               (0) */
+    NotDefinedYet,            /*    None                               (0) */
+    NotDefinedYet,            /*    None                               (0) */
+    NotDefinedYet,            /*    None                               (0) */
+    NotDefinedYet,            /*    None                               (0) */
+    NotDefinedYet,            /*    None                               (0) */
+    NotDefinedYet,            /*    None                               (0) */
+    NotDefinedYet,            /*    None                               (0) */
+    NotDefinedYet,            /*    None                               (0) */
+    NotDefinedYet,            /*    None                               (0) */
+    NotDefinedYet,            /*    None                               (0) */
+    NotDefinedYet,            /*    None                               (0) */
+    NotDefinedYet,            /*    None                               (0) */
+    NotDefinedYet,            /*    None                               (0) */
+    NotDefinedYet,            /*    None                               (0) */
+    NotDefinedYet,            /*    None                               (0) */
+    NotDefinedYet,            /*    None                               (0) */
+    NotDefinedYet,            /*    None                               (0) */
+    NotDefinedYet,            /*    None                               (0) */
+    NotDefinedYet,            /*    None                               (0) */
+    NotDefinedYet,            /*    None                               (0) */
+    NotDefinedYet,            /*    None                               (0) */
+    NotDefinedYet,            /*    None                               (0) */
+    NotDefinedYet,            /*    None                               (0) */
+    NotDefinedYet,            /*    None                               (0) */
+    NotDefinedYet,            /*    None                               (0) */
+    wfaStaScan
 };
 
 
