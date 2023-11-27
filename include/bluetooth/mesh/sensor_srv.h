bfabfabded2fbe89fae3cb485421381b77137399
Alperen Şener <alperen.sener@nordicsemi.no>
2023-11-27 16:13:27 +0100

    bluetooth: mesh: update bt_mesh_model struct usage
    Updating bt_mesh_model struct usage.
    
    Signed-off-by: Alperen Şener <alperen.sener@nordicsemi.no>

--- include/bluetooth/mesh/sensor_srv.h
+++ include/bluetooth/mesh/sensor_srv.h
@@ -112,7 +112,7 @@ struct bt_mesh_sensor_srv {
  *
  *  @return 0 on success, or (negative) error code otherwise.
  */
-int bt_mesh_sensor_srv_pub(const struct bt_mesh_sensor_srv *srv,
+int bt_mesh_sensor_srv_pub(struct bt_mesh_sensor_srv *srv,
 			   struct bt_mesh_msg_ctx *ctx,
 			   struct bt_mesh_sensor *sensor,
 			   const struct sensor_value *value);
@@ -137,7 +137,7 @@ int bt_mesh_sensor_srv_pub(const struct bt_mesh_sensor_srv *srv,
  *                         not configured.
  *  @retval -EAGAIN        The device has not been provisioned.
  */
-int bt_mesh_sensor_srv_sample(const struct bt_mesh_sensor_srv *srv,
+int bt_mesh_sensor_srv_sample(struct bt_mesh_sensor_srv *srv,
 			      struct bt_mesh_sensor *sensor);
 
 /** @cond INTERNAL_HIDDEN */
